90fb1ff3643685369d3986f5452101fff72ffafb
KANG-g-u <61117544+KANG-g-u@users.noreply.github.com>
2020-08-12 22:23:44 +0900

    Create 1927.cpp

---
+++ 1927.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+	priority_queue<int, vector<int>,greater<int> > pq;
+	int n;
+	cin>>n;
+	
+	int temp;
+	for(int i=0;i<n;i++){
+		scanf("%d",&temp);
+		if(temp==0){
+			if(pq.empty())
+				printf("0\n");
+			else{
+				printf("%d\n",pq.top());
+				pq.pop();
+			}
+		}
+		else{
+			pq.push(temp);
+		}
+	}
+	return 0;
+}
